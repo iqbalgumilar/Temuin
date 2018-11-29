ab8976f0349413161b87a865261f8bb247e855ef
Alfi Gusman <alfigusman0@gmail.com>
2018-11-29 17:40:05 +0700

    update source code model

--- app/MasterProduk.php
+++ app/MasterProduk.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class MasterProduk extends Model
 {
-    //
+    protected $table = 'master_produks';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'id_jenis_produk',
+        'produk',
+        'file_produk',
+        'status',
+        'harga_produk',
+        'created_by',
+        'updated_by'
+    ];
 }
--- app/Services.php
+++ app/Services.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Services extends Model
 {
-    //
+    protected $table = 'services';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'id_profile',
+        'uid_service',
+        'icon_service',
+        'descripsion_service'
+    ];
 }

77346b43c23470ff48e510fe0d5b2090b35f62b6
root <alfigusman0@gmail.com>
2018-12-25 18:46:44 +0700

    add id_profile field

--- database/migrations/2018_12_25_101054_create_themes_table.php
+++ database/migrations/2018_12_25_101054_create_themes_table.php
@@ -15,9 +15,11 @@ class CreateThemesTable extends Migration
     {
         Schema::create('themes', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('id_profile');
             $table->unsignedInteger('uid_pb');
             $table->unsignedInteger('uid_cv');
             $table->unsignedInteger('uid_kn');
+            $table->foreign('id_profile')->references('id')->on('profiles')->onUpdate('cascade');
             $table->foreign('uid_pb')->references('id')->on('master_produks')->onUpdate('cascade');
             $table->foreign('uid_cv')->references('id')->on('master_produks')->onUpdate('cascade');
             $table->foreign('uid_kn')->references('id')->on('master_produks')->onUpdate('cascade');
